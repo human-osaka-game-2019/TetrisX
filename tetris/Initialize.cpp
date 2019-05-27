a22a13a8610a3f056ad90b4232d3520c2e4aeb6a
kazuma_ohura <k.ohura0303@gmail.com>
2019-05-27 17:48:47 +0900

    test

---
+++ tetris/Initialize.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <d3d9.h>
+#include <dinput.h>
+#include <tchar.h>
+
+#pragma comment(lib,"d3d9.lib")
+#pragma comment(lib,"d3dx9.lib")
+#pragma comment(lib,"dinput8.lib")
+#pragma comment(lib,"dxguid.lib")
+#pragma comment(lib,"winmm.lib")
+
+INT WINAPI WinMain(HINSTANCE hinst, HINSTANCE hPrevInstance, PSTR lpCmdline, int nCmdShow) {
+	MSG msg;
+}
