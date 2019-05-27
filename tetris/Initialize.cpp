89cab447ff77c018e793e4303ababd7d37a8cb25
kazuma_ohura <k.ohura0303@gmail.com>
2019-05-27 18:35:56 +0900

    全部追加

--- tetris/Initialize.cpp
+++ tetris/Initialize.cpp
@@ -9,6 +9,42 @@
 #pragma comment(lib,"dxguid.lib")
 #pragma comment(lib,"winmm.lib")
 
-INT WINAPI WinMain(HINSTANCE hinst, HINSTANCE hPrevInstance, PSTR lpCmdline, int nCmdShow) {
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp);
+
+const TCHAR API_NAME[] = _T("Tetris_X");
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdline, int nCmdShow) {
 	MSG msg;
+	WNDCLASS Wndclass;
+	Wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	Wndclass.lpfnWndProc = WndProc;
+	Wndclass.cbClsExtra = 0;
+	Wndclass.cbWndExtra = 0;
+	Wndclass.hInstance = hInstance;
+	Wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	Wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	Wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	Wndclass.lpszClassName = NULL;
+	Wndclass.lpszClassName = API_NAME;
 }
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
+	switch (msg) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	case WM_SYSKEYDOWN:
+		switch (wp) {
+		case VK_RETURN:
+			return 0;
+		case VK_F4:
+			PostMessage(hWnd, WM_CLOSE, 0, 0);
+			return 0;
+		default:
+			return 0;
+		}
+		return 0;
+	}
+	return DefWindowProc(hWnd, msg, wp, lp);
+}
+
