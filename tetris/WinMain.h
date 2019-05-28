9836a85c737d2dd43eb468af027bf0032abf9d60
MinoriNakamura <saintlyterritory526@gmail.com>
2019-05-28 17:29:50 +0900

    カスタムバーテックスを追加

--- tetris/WinMain.h
+++ tetris/WinMain.h
@@ -1,6 +1,13 @@
 #include <windows.h>
 #include <d3d9.h>
 
+//カスタムバーテックス
+struct CustomVertex {
+	FLOAT x, y, z, rhw;
+	DWORD color;
+	FLOAT tu, tv;
+};
+
 //WinMain関数
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdline, int nCmdShow);
 
