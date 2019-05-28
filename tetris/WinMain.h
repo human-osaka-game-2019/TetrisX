2db471dfa6d973a1ccad607509a486365d9a0950
kazuma_ohura <k.ohura0303@gmail.com>
2019-05-28 09:03:08 +0900

    Initialize.cppの内容をWinMain.cppに移動

---
+++ tetris/WinMain.h
@@ -0,0 +1,8 @@
+#include <windows.h>
+#include <d3d9.h>
+
+//WinMain関数
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdline, int nCmdShow);
+
+//ウィンドプロシージャの生成
+LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
