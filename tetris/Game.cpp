4fe7ba45781e7a1f5512c9d61db2308e2b3d790e
taishi-matsumura <taishi0706@gmail.com>
2019-07-02 10:21:01 +0900

    :sparkles: 積み上げられるように大改造

--- tetris/Game.cpp
+++ tetris/Game.cpp
@@ -35,161 +35,41 @@ void Game::Loading() {
 
 	//テトリミノをランダム
 	srand((unsigned int)time(NULL));
-
-	//初期化する関数にする
-	for (INT row = 0; row < 20; row++) {
-		for (INT col = 0; col < 10; col++) {
-			exists[row][col] = false;
-		}
-	}
+	CreateMino();
 
 	Phase = PROCESSING;
 }
 
 //ゲームの描画と動き
 void Game::Process() {
-
 	FrameCount++;
 
+	Control();
 	
-	//一瞬で下に落とす処理
-	if (dx.KeyState[DIK_UP] == dx.PRESS) {
-
-		block_y -= 36;
-
-	}
-	//下移動
-	if (dx.KeyState[DIK_DOWN] == dx.PRESS) {
-
-		block_y += 36;
-
-	}
-	//右移動
-	if (dx.KeyState[DIK_RIGHT] == dx.PRESS) {
-
-		block_x += 36;
-
-	}
-	//左移動
-	if (dx.KeyState[DIK_LEFT] == dx.PRESS) {
-
-		block_x -= 36;
-
-	}
-
 	//ゲーム時の背景描画
-	Draw(0, 0, 0.0f, 0.0f, window_width, window_height, 1.0f, 1.0f, GAME_BACK);
+	Draw(0, 0, 0.0f, 0.0f, windowSize.w, windowSize.h, 1.0f, 1.0f, GAME_BACK);
 	
 	//ステージ描画
-	for (INT row = 0; row < 20; row++) {
-
-		for (INT col = 0; col < 10; col++) {
-			
-			board[row][col];
-
-			switch(color){
-
-			case Back:
-				block_tu = 0.75f;
-				block_tv = 0.75f;
-				exists[row][col] = false;
-				break;
-			case Red:
-				block_tu = 0.0f;
-				block_tv = 0.0f;
-				exists[row][col] = true;
-				break;
-			case Blue:
-				block_tu = 0.25f;
-				block_tv = 0.0f;
-				exists[row][col] = true;
-				break;
-			case Green:
-				block_tu = 0.5f;
-				block_tv = 0.0f;
-				exists[row][col] = true;
-				break;
-			case Yellow:
-				block_tu = 0.75f;
-				block_tv = 0.0f;
-				exists[row][col] = true;
-				break;
-			case White:
-				block_tu = 0.0f;
-				block_tv = 0.25;
-				exists[row][col] = true;
-				break;
-			case Black:
-				block_tu = 0.25f;
-				block_tv = 0.25f;
-				exists[row][col] = true;
-				break;
-			case Lightblue:
-				block_tu = 0.75f;
-				block_tv = 0.25f;
-				exists[row][col] = true;
-				break;
-			default:
-				block_tu = 0.75f;
-				block_tv = 0.75f;
-				exists[row][col] = false;
-				break;
-			}
+	for (INT row = 4; row < 25; row++) {
+		for (INT col = 0; col < 12; col++) {
+			UV uv = GetUV(board[row][col]);
 
 			//二次元配列にし、背景に反映→動いているブロックから情報を持ってくる
 					 	//座標の特性持った構造体を作る
-			Draw(stage_x + stage_block_width * col, stage_y + stage_block_hight * row, block_tu, block_tv, stage_block_width, stage_block_hight,0.25f,0.25f, GAME_BLOCK);
-
+			FLOAT x = stageTopLeft.x + blockSize.w * col;
+			FLOAT y = stageTopLeft.y + blockSize.h * row;
+			Draw(x, y, uv.u, uv.v, blockSize.w, blockSize.h, 0.25f, 0.25f, GAME_BLOCK);
 		}
 	}
 
-	//当たり判定
-	Jugement();
-
-	//ブロック描画
-	DrawBlocks();
-
-	//重力
-	if (FrameCount == 60){
-		// 1秒たった
-		FrameCount = 0;
-		block_y += 36;
-	}
+	//ミノ描画
+	DrawMino();
 
 	//下につくと7種類の描画・真ん中に移動処理
-	if (block_y + block_height >= window_height - 36) {
-
-		switch (block_kind) {
-		case L: 
-			color = Red;
-			break;
-		case I:
-			color = Blue;
-			break;
-		case J:
-			color = Green;
-			break;
-		case S:
-			color = Yellow;
-			break;
-		case O:
-			color = White;
-			break;
-		case Z:
-			color = Black;
-			break;
-		case T:
-			color = Lightblue;
-		}
-		block_kind = rand() % 7;
-		block_x = stage_x + 108;
-		block_y = -144;
+	if (landed) {
+		FixMino();
+		CreateMino();
 	}
-
-	
-
-
-
 
 	//debug用
 	if (dx.KeyState[DIK_RETURN] == dx.PRESS) {
@@ -197,12 +77,65 @@ void Game::Process() {
 	}
 }
 
+void Game::Control()
+{
+	//一瞬で下に落とす処理
+	if (dx.KeyState[DIK_UP] == dx.PRESS) {
+		//block_y -= 36;
+	}
+	//下移動
+	if (dx.KeyState[DIK_DOWN] == dx.PRESS){
+		MoveDown();
+	}
+	//右移動
+	if (dx.KeyState[DIK_RIGHT] == dx.PRESS){
+		MoveHorizontally(RIGHT);
+	}
+	//左移動
+	if (dx.KeyState[DIK_LEFT] == dx.PRESS) {
+		MoveHorizontally(LEFT);
+	}
+
+	//重力
+	if (FrameCount == 60){
+		// 1秒たった
+		FrameCount = 0;
+		MoveDown();
+	}
+}
+
+void Game::MoveDown()
+{
+	if (CanMove(DOWN)) {
+		++blockPosition.row;
+	} else {
+		landed = true;
+	}
+}
+
+void Game::MoveHorizontally(DIRECTION direction)
+{
+	if(CanMove(direction)) {
+		blockPosition.col += (direction == LEFT ? -1 : 1);
+	}
+}
+
+void Game::FixMino()
+{
+	int col = blockPosition.col;
+	int row = blockPosition.row;
+
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			if (mino[y][x] != 0) {
+				board[row + y][col + x] = minoColor;
+			}
+		}
+	}
+}
+
 //ゲームのテクスチャの解放
 void Game::Release() {
-	//ブロック位置の初期化
-	block_x = stage_x + 108;
-	block_y = -144;
-
 	//テクスチャの開放
 	dx.pTexture[GAME_BACK]->Release();
 	dx.pTexture[GAME_BACK] = nullptr;
@@ -213,143 +146,141 @@ void Game::Release() {
 	scene = RESULT;
 }
 
-void Game::DrawBlocks() {
+void Game::CreateMino() {
+	const INT(*tmpl)[4];
 
-	/*ブロック描画*/
+	switch (rand() % 7) {
+	case L:
+		minoColor = Red;
+		tmpl = MINO_L;
+		break;
+	case I:
+		minoColor = Blue;
+		tmpl = MINO_I;
+		break;
+	case J:
+		minoColor = Green;
+		tmpl = MINO_J;
+		break;
+	case S:
+		minoColor = Yellow;
+		tmpl = MINO_S;
+		break;
+	case O:
+		minoColor = White;
+		tmpl = MINO_O;
+		break;
+	case Z:
+		minoColor = Black;
+		tmpl = MINO_Z;
+		break;
+	case T:
+		minoColor = Lightblue;
+		tmpl = MINO_T;
+		break;
+	default:
+		return;
+	}
 
-	if (block_kind == L) {
-		//Lのブロック
-		for (block_row = 0; block_row < 4; block_row++) {
-
-			for (block_col = 0; block_col < 4; block_col++) {
-				if ((block_col == 1 && block_row < 3) || (block_row == 2 && block_col < 3 && block_col > 0)) {
-
-					Draw(block_x + block_width * block_col, block_y + block_height * block_row, 0.0f, 0.0f, block_width, block_height, 0.25f, 0.25f, GAME_BLOCK);
-					
-					/*if (board[20][1] == Back) {
-						board[20][1] = Red;
-						board[20][2] = Red;
-						board[19][1] = Red;
-						board[18][1] = Red;
-						exists[20][1] = true;
-						exists[20][1] = true;
-						exists[20][1] = true;
-						exists[20][1] = true;
-					}*/
-				}
-			}
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			mino[y][x] = tmpl[y][x];
 		}
 	}
-	else if (block_kind == L) {
 
-	}
-	else if (block_kind == I) {
-		//Iのブロック
-		for (INT block_row = 0; block_row < 4; block_row++) {
+	blockPosition.col = 3;
+	blockPosition.row = 0;
+	landed = false;
+}
 
-			for (INT block_col = 0; block_col < 4; block_col++) {
-				if (block_col == 1) {
-
-					Draw(block_x + block_width * block_col, block_y + block_height * block_row, 0.25f, 0.0f, block_width, block_height, 0.25f, 0.25f, GAME_BLOCK);
-
-				}
-			}
-		}
-	}
-	else if (block_kind == J) {
-		//Jのブロック
-		for (INT block_row = 0; block_row < 4; block_row++) {
-
-			for (INT block_col = 0; block_col < 4; block_col++) {
-				if ((block_col == 2 && block_row < 3) || (block_row == 2 && block_col < 3 && block_col >0)) {
-
-					Draw(block_x + block_width * block_col, block_y + block_height * block_row, 0.5f, 0.0f, block_width, block_height, 0.25f, 0.25f, GAME_BLOCK);
-				}
-			}
-		}
-	}
-	else if (block_kind == S) {
-		//Sのブロック
-		for (INT block_row = 0; block_row < 4; block_row++) {
-
-			for (INT block_col = 0; block_col < 4; block_col++) {
-				if ((block_col == 1 && block_row == 1) || (block_col == 2 && block_row == 1) || (block_col == 0 && block_row == 2) || (block_col == 1 && block_row == 2)) {
-
-					Draw(block_x + block_width * block_col, block_y + block_height * block_row, 0.75f, 0.0f, block_width, block_height, 0.25f, 0.25f, GAME_BLOCK);
-
-				}
-			}
-		}
-	}
-	else if (block_kind == O) {
-		//Oのブロック
-		for (INT block_row = 0; block_row < 4; block_row++) {
-
-			for (INT block_col = 0; block_col < 4; block_col++) {
-				if ((block_col == 1 || block_col == 2) && (block_row == 1 || block_row == 2)) {
-
-					Draw(block_x + block_width * block_col, block_y + block_height * block_row, 0.0f, 0.25f, block_width, block_height, 0.25f, 0.25f, GAME_BLOCK);
-
-				}
-			}
-		}
-	}
-	else if (block_kind == Z) {
-		//Zのブロック
-		for (INT block_row = 0; block_row < 4; block_row++) {
-
-			for (INT block_col = 0; block_col < 4; block_col++) {
-				if ((block_col == 0 && block_row == 1) || (block_col == 1 && block_row == 1) || (block_col == 1 && block_row == 2) || (block_col == 2 && block_row == 2)) {
-
-					Draw(block_x + block_width * block_col, block_y + block_height * block_row, 0.25f, 0.25f, block_width, block_height, 0.25f, 0.25f, GAME_BLOCK);
-
-				}
-			}
-		}
-	}
-	else if (block_kind == T) {
-		//Tのブロック
-		for (INT block_row = 0; block_row < 4; block_row++) {
-
-			for (INT block_col = 0; block_col < 4; block_col++) {
-				if ((block_col == 0 && block_row == 1) || (block_col == 1 && block_row == 1) || (block_col == 2 && block_row == 1) || (block_col == 1 && block_row == 2)) {
-
-					Draw(block_x + block_width * block_col, block_y + block_height * block_row, 0.75f, 0.25f, block_width, block_height, 0.25f, 0.25f, GAME_BLOCK);
-
-				}
+void Game::DrawMino() {
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			if (mino[y][x]) {
+				FLOAT left = stageTopLeft.x + blockSize.w * (blockPosition.col + x);
+				FLOAT top = stageTopLeft.y + blockSize.h * (blockPosition.row + y);
+				UV uv = GetUV(minoColor);
+				Draw(left, top, uv.u, uv.v, blockSize.w, blockSize.h, 0.25f, 0.25f, GAME_BLOCK);
 			}
 		}
 	}
 }
 
-void Game::Jugement() {
+Game::UV Game::GetUV(COLOR color)
+{
+	UV uv;
+	switch (color) {
+	case Red:
+		uv.SetUV(0.0f, 0.0f);
+		break;
+	case Blue:
+		uv.SetUV(0.25f, 0.0f);
+		break;
+	case Green:
+		uv.SetUV(0.5f, 0.0f);
+		break;
+	case Yellow:
+		uv.SetUV(0.75f, 0.0f);
+		break;
+	case White:
+		uv.SetUV(0.0f, 0.25f);
+		break;
+	case Black:
+		uv.SetUV(0.5f, 0.25f);
+		break;
+	case Lightblue:
+		uv.SetUV(0.75f, 0.25f);
+		break;
+	case Wall:
+		uv.SetUV(0.25f, 0.5f);
+		break;
+	default:
+		uv.SetUV(0.75f, 0.75f);
+		break;
+	}
 
-	//当たり判定・左・L
-	if (block_x <= stage_x - stage_block_width && (block_kind == L || block_kind == I || block_kind == J || block_kind == O)) {
-		block_x = stage_x - stage_block_width;
-	}
-	else if (block_x <= stage_x && (block_kind == S || block_kind == Z || block_kind == T)) {
-		block_x = stage_x;
-	}
-
-	//当たり判定・右
-	if (block_x >= (stage_x + stage_width) - stage_block_width * 3 && block_kind != I) {
-		block_x = stage_x + stage_width - stage_block_width * 3;
-	}
-	else if (block_x >= (stage_x + stage_width) - stage_block_width * 2 && block_kind == I) {
-		block_x = (stage_x + stage_width) - stage_block_width * 2;
-	}
+	return uv;
 }
 
-//* ブロックを位置情報に従ってフィールドにコピーする */
-//void GAME::PIECE::PieceToField()
-//{
-//	for (int y = 0; y < PIECE_HEIGHT; y++) {
-//		for (int x = 0; x < PIECE_WIDTH; x++) {		// ↓(location.y)+y>=0 は添字の有効性を調べている
-//			if (piece[x][y] && (location.y) + y >= 0) {
-//				field[(location.x) + x][(location.y) + y] = piece[x][y];
-//				fColor[(location.x) + x][(location.y) + y] = pColor[x][y];
-//			}
-//		}
-//	}
-//}
+bool Game::CanMove(DIRECTION direction)
+{
+	// 衝突判定の基点となるCell
+	Cell topLeft = blockPosition;
+
+	switch (direction) {
+	case Game::LEFT:
+	case Game::RIGHT:
+		// ミノを左右に動かした場所で衝突しているかをチェックする
+		topLeft.col += (direction == RIGHT ? 1 : -1);
+		break;
+	case Game::DOWN:
+		// ミノを下に動かした場所で衝突しているかをチェックする
+		++topLeft.row;
+		break;
+	default:
+		return false;
+	}
+
+	if (Collides(topLeft)) {
+		return false;
+	}
+
+	return true;
+}
+
+bool Game::Collides(Cell topLeft)
+{
+	for (int y = 0; y < 4; y++) {
+		for (int x = 0; x < 4; x++) {
+			if (mino[y][x] == Back) {
+				continue;
+			}
+
+			if (board[topLeft.row + y][topLeft.col + x] != Back) {
+				return true;
+			}
+		}
+	}
+
+	return false;
+}
--- tetris/Game.h
+++ tetris/Game.h
@@ -19,51 +19,6 @@ public:
 	Game::SCENE_PAHSE Phase = Game::LOAD;
 
 private:
-
-	//ウィンドウのサイズ
-	FLOAT window_height = 720;
-	FLOAT window_width = 1280;
-
-	//ゲームステージの位置
-	FLOAT stage_x = 460;
-	FLOAT stage_y = 0;
-	FLOAT stage_width = 360;
-	FLOAT stage_height = 720;
-
-	//ステージの1マスの大きさ
-	FLOAT stage_block_width = 36;
-	FLOAT stage_block_hight = 36;
-
-	//移動ブロックの位置
-	FLOAT block_x = stage_x + 108;
-	FLOAT block_y = -144;
-
-	//ブロックの1マスの大きさ
-	FLOAT block_width = 36;
-	FLOAT block_height = 36;
-		   
-	//フレームを数える
-	INT FrameCount = 0;
-
-	//ブロックの種類
-	INT block_kind = 0;
-
-	//移動ブロックの縦と横
-	INT block_row = 0;
-	INT block_col = 0;
-
-	//ブロックのtuとtv
-	FLOAT block_tu = 0.5f;
-	FLOAT block_tv = 0.5f;
-
-	//関数のプロトタイプ宣言
-	void Loading();
-	void Process();
-	void Release();
-	void DrawBlocks();
-	void Jugement();
-
-	//
 	enum COLOR {
 		Back,
 		Red,
@@ -76,8 +31,6 @@ private:
 		Wall,
 	};
 
-	COLOR color = Back;
-
 	//ブロックの種類
 	enum BLOCK_KIND {
 		L,
@@ -89,7 +42,75 @@ private:
 		T
 	};
 
-	INT board[21][12] = {
+	// 方向
+	enum DIRECTION {
+		LEFT,
+		RIGHT,
+		DOWN
+	};
+
+	// 行と列
+	struct Cell {
+		int row, col;
+	};
+
+	// 座標(pixel)
+	struct Point {
+		FLOAT x, y;
+	};
+
+	// uv値
+	struct UV {
+		FLOAT u, v;
+		void SetUV(FLOAT tu, FLOAT tv) { u = tu; v = tv; }
+	};
+
+	// 幅と高さ(pixel)
+	struct Size {
+		FLOAT w, h;
+	};
+
+	//関数のプロトタイプ宣言
+	void Loading();
+	void Process();
+	void Control();
+	void Release();
+	void CreateMino();
+	void DrawMino();
+	UV GetUV(COLOR color);
+	bool CanMove(DIRECTION direction);
+	bool Collides(Cell topLeft);
+	void MoveDown();
+	void MoveHorizontally(DIRECTION direction);
+	void FixMino();
+
+	//ウィンドウのサイズ
+	Size windowSize = { 1280.0f, 720.0f };
+
+	//ゲームステージの位置
+	Point stageTopLeft = { 460.0f, -144.0f };
+
+	//ブロックの1マスの大きさ
+	Size blockSize = { 36.0f, 36.0f };
+		   
+	//フレームを数える
+	INT FrameCount = 0;
+
+	//移動ブロックの縦と横
+	Cell blockPosition;
+
+	// 接地したかどうか
+	bool landed = false;
+
+	// 落下中のminoの色
+	COLOR minoColor = Red;
+
+	COLOR board[25][12] = {
+	{Wall,Back,Back,Back,Back,Back,Back,Back,Back,Back,Back,Wall},
+	{Wall,Back,Back,Back,Back,Back,Back,Back,Back,Back,Back,Wall},
+	{Wall,Back,Back,Back,Back,Back,Back,Back,Back,Back,Back,Wall},
+	{Wall,Back,Back,Back,Back,Back,Back,Back,Back,Back,Back,Wall},
+	{Wall,Back,Back,Back,Back,Back,Back,Back,Back,Back,Back,Wall},
 	{Wall,Back,Back,Back,Back,Back,Back,Back,Back,Back,Back,Wall},
 	{Wall,Back,Back,Back,Back,Back,Back,Back,Back,Back,Back,Wall},
 	{Wall,Back,Back,Back,Back,Back,Back,Back,Back,Back,Back,Wall},
@@ -112,42 +133,56 @@ private:
 	{Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall,Wall},
 	};
 
-	//ステージのフラグ
-	bool exists[21][12] = {
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,false,false,false,false,false,false,false,false,false,false,true},
-		{true,true ,true ,true ,true ,true ,true ,true ,true ,true ,true ,true},
+	const int MINO_L[4][4] = {
+		{0, 1, 0, 0},
+		{0, 1, 0, 0},
+		{0, 1, 1, 0},
+		{0, 0, 0, 0},
 	};
 
-	// ピース管理構造体
-	typedef struct tagPIECE
-	{
-		//bool block[block_row][block_col]; // true：ブロックあり / false：なし
-		BYTE image;         // ブロックのビットマップ番号
-		char x, y;           // 左上のセル座標
-	}PIECE;
+	const int MINO_I[4][4] = {
+		{0, 1, 0, 0},
+		{0, 1, 0, 0},
+		{0, 1, 0, 0},
+		{0, 1, 0, 0},
+	};
 
-	//現在移動中のブロックの位置 
-	PIECE location = { 0,0 };
+	const int MINO_J[4][4] = {
+		{0, 0, 1, 0},
+		{0, 0, 1, 0},
+		{0, 1, 1, 0},
+		{0, 0, 0, 0},
+	};
 
+	const int MINO_S[4][4] = {
+		{0, 0, 0, 0},
+		{0, 1, 1, 0},
+		{1, 1, 0, 0},
+		{0, 0, 0, 0},
+	};
+
+	const int MINO_O[4][4] = {
+		{0, 0, 0, 0},
+		{0, 1, 1, 0},
+		{0, 1, 1, 0},
+		{0, 0, 0, 0},
+	};
+
+	const int MINO_Z[4][4] = {
+		{0, 0, 0, 0},
+		{1, 1, 0, 0},
+		{0, 1, 1, 0},
+		{0, 0, 0, 0},
+	};
+
+	const int MINO_T[4][4] = {
+		{0, 0, 0, 0},
+		{1, 1, 1, 0},
+		{0, 1, 0, 0},
+		{0, 0, 0, 0},
+	};
+
+	int mino[4][4];
 };
 
 #endif
